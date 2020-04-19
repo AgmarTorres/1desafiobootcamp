fdd339f211c2c79a269fc717fcdc02e808207f29
Agmar Torres <agmartorres@gmail.com>
2020-04-19 16:23:39 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -11,23 +11,72 @@ app.use(cors());
 const repositories = [];
 
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+  const repository = {
+    id: uuid(),
+    title,
+    url,
+    techs,
+    likes: 0,
+  };
+
+  repositories.push(repository);
+  return response.json(repository);
 });
 
 app.put("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const { title, url, techs } = request.body;
+  const indexRepository = repositories.findIndex(
+    (repository) => repository.id == id
+  );
+
+  if (indexRepository == -1) {
+    return response.status(400).json({ error: "Esse repositorio nao existe" });
+  }
+
+  const repository = {
+    id,
+    title,
+    url,
+    techs,
+    likes: repositories[indexRepository].likes,
+  };
+  repositories[indexRepository] = repository;
+
+  return response.json(repository);
 });
 
 app.delete("/repositories/:id", (request, response) => {
-  // TODO
+  const { id } = request.params;
+
+  const findindexOfRepository = repositories.findIndex(
+    (repository) => repository.id == id
+  );
+
+  if (findindexOfRepository > 0) {
+    repositories.splice(findindexOfRepository, 1);
+  } else {
+    return response.status(400).json({ error: "Repositório não existe" });
+  }
+
+  return response.status(204).send();
 });
 
 app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+  const { id } = request.params;
+  const repository = repositories.find((repository) => repository.id == id);
+
+  if (!repository) {
+    return response.status(400).json({ error: "Esse repositorio nao existe" });
+  }
+
+  repository.likes += 1;
+  return response.json(repository);
 });
 
 module.exports = app;
